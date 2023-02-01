c3f72b0590502366e2781a9ab31010573a6935de
Duck1s <joeri.breedveld123@gmail.com>
2023-02-01 09:30:22 +0100

    feat(responsive): start on responsive with tailwind

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,9 +5,27 @@ const Services = () => {
   return (
     <section className="page-width flex flex-col gap-8 py-16">
       <h2>Services</h2>
-      <Card title="Photography" button="explore" href="/" img={placeholder} />
-      <Card title="3D Design" button="explore" href="/" img={placeholder} />
-      <Card title="UX/UI Design" button="explore" href="/" img={placeholder} />
+      <ul className="flex flex-col justify-between md:flex-row">
+        <li>
+          <Card
+            title="Photography"
+            button="explore"
+            href="/"
+            img={placeholder}
+          />
+        </li>
+        <li>
+          <Card title="3D Design" button="explore" href="/" img={placeholder} />
+        </li>
+        <li>
+          <Card
+            title="UX/UI Design"
+            button="explore"
+            href="/"
+            img={placeholder}
+          />
+        </li>
+      </ul>
     </section>
   );
 };
